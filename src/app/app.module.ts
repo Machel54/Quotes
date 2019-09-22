15aabc34ae6c5e96dc02d0a7e8b48808cac35e02
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-09-23 00:48:04 +0300

    add likean dislike

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { LikeComponent } from './like/like.component';
 
 
 
@@ -18,6 +19,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     QuoteDetailsComponent,
     DateCountPipe,
     QuoteFormComponent,
+    LikeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/like/like.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from './../quote';
+
+@Component({
+  selector: 'app-like',
+  templateUrl: './like.component.html',
+  styleUrls: ['./like.component.css']
+})
+export class LikeComponent implements OnInit {
+   
+  numberOfLikes : number = 0;
+  quote: any;
+  
+  likeButtonClick() {
+    this.numberOfLikes++;
+  }
+
+  dislikeButtonClick() {
+    this.numberOfLikes--;
+  }
+
+  voteUp(id:number){
+    this.quote[id].upVote()
+
+  }
+  voteDown(id:number){
+    this.quote[id].downVote()
+
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
