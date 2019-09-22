fd8efde3815eb24b63fda2bf45260e7e63ea6f42
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-09-22 19:17:36 +0300

    add date pipe

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,6 @@
 export class Quote {
-  
-  showDescription: boolean;
-  constructor(public id: number,public description: string,public name: string){
+  public showDescription: boolean;
+  constructor(public id: number,public description: string,public name: string, public completeDate: Date){
     this.showDescription=false;
   }
 }
