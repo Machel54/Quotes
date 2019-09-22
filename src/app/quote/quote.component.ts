387d5844fea44e6d865de229a9699db0b3d8cff1
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-09-22 23:35:34 +0300

    form submit functional

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,6 +15,12 @@ export class QuoteComponent implements OnInit {
     new Quote(4,'Mahatma Gandhi','I will not let anyone walk through my mind with their dirty feet.',new Date(2019,0,18)),
     new Quote(5,'Pablo Picasso','The purpose of art is washing the dust of daily life off our souls.',new Date(2019,2,14)),
   ];
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
 
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
